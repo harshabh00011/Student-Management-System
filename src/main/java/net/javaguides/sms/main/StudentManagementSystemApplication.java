7e191d0e25107a65367a4ca3bcc6f54605d4b8b3
Harshabh Gupta <harshabh.gupta09@gmail.com>
2024-01-09 01:08:05 +0530

    first commit

---
+++ src/main/java/net/javaguides/sms/main/StudentManagementSystemApplication.java
@@ -0,0 +1,27 @@
+package net.javaguides.sms.main;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import net.javaguides.sms.entity.Student;
+import net.javaguides.sms.repository.StudentRepository;
+
+@SpringBootApplication
+public interface StudentManagementSystemApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(StudentManagementSystemApplication.class, args);
+	}
+
+	@Autowired
+	private StudentRepository studentRepository;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		
+				
+	}
+	
+}
